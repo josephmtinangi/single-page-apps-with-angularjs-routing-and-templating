33ed915c7d1fe176aae9c730d3536197850ea678
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-01-09 20:55:05 +0300

    Configure routes and views

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 // script.js
 
    // create the module and name it mtinangiApp
-   var mtinangiApp = angular.module('mtinangiApp', []);
+   // also include ngRoute for all our routing needs
+   var mtinangiApp = angular.module('mtinangiApp', ['ngRoute']);
+
+   // configure my routes
+   mtinangiApp.config(function($routeProvider) {
+    $routeProvider
+
+    // route for the home page
+    .when('/', {
+      templateUrl : 'pages/home.html',
+      controller : 'mainController'
+    })
+
+    // route for the about page
+    .when('/about', {
+      templateUrl : 'pages/about.html',
+      controller : 'aboutController'
+    })
+
+    // route for the contact page
+    .when('/contact', {
+      templateUrl : 'pages/contact.html',
+      controller : 'contactController'
+    });
+
+   });
 
    // create the controller and inject Angular's $scope
    mtinangiApp.controller('mainController', function($scope) {
@@ -9,3 +34,11 @@
       // create a message to display in our view
       $scope.message = 'Everyone come and see how good I look!';
    });
+
+   mtinangiApp.controller('aboutController', function($scope) {
+    $scope.message = 'Look I am an about page.';
+   });
+
+   mtinangiApp.controller('contactController', function($scope) {
+    $scope.message = 'Contact us! JK. This is just a demo.';
+   })
